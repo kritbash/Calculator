ef7ce8892775c0b99c69ebd1a834ea8c82d5118a
kritbash <144454185+kritbash@users.noreply.github.com>
2023-09-08 18:29:16 +0530

    Three files are uploaded
    This repository consisting of three files.

---
+++ script.js
@@ -0,0 +1,37 @@
+const box = document.getElementById("displaybox");
+document.getElementById("box1").addEventListener("click",function(){
+    console.log("clicked box1");
+    box.innerText = "1"
+})
+document.getElementById("box2").addEventListener("click",function(){
+    console.log("clicked box1");
+    box.innerText = "2"
+})
+document.getElementById("box3").addEventListener("click",function(){
+    console.log("clicked box1");
+    box.innerText = "3"
+})
+document.getElementById("box4").addEventListener("click",function(){
+    console.log("clicked box1");
+    box.innerText = "4"
+})
+document.getElementById("box5").addEventListener("click",function(){
+    console.log("clicked box1");
+    box.innerText = "5"
+})
+document.getElementById("box6").addEventListener("click",function(){
+    console.log("clicked box1");
+    box.innerText = "6"
+})
+document.getElementById("box7").addEventListener("click",function(){
+    console.log("clicked box1");
+    box.innerText = "7"
+})
+document.getElementById("box8").addEventListener("click",function(){
+    console.log("clicked box1");
+    box.innerText = "8"
+})
+document.getElementById("box9").addEventListener("click",function(){
+    console.log("clicked box1");
+    box.innerText = "9"
+})
